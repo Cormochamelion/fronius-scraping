f48149cd6e2e43d65b7e295d85f818aa3c7bb629
cormochamelion <cormocodran1.5@gmail.com>
2024-05-20 21:59:54 +0200

    fronius_scraper: Add script for data parsing

---
+++ fronius_scraper/scripts/parse_json_files.py
@@ -0,0 +1,36 @@
+import argparse
+
+from fronius_scraper import data_parser
+
+
+def main():
+    argparser = argparse.ArgumentParser(
+        "fronius-parse-json-files",
+        description=("Parse scraped JSON files into a usable format."),
+    )
+
+    argparser.add_argument(
+        "--input-dir",
+        "-i",
+        default="./",
+        type=str,
+        # TODO Add info on filename pattern.
+        help="Dir in which to search for JSON files to parse (default: %(default)s).",
+    )
+
+    argparser.add_argument(
+        "--output-dir",
+        "-o",
+        default="./",
+        type=str,
+        # TODO Add info on what will be outputted.
+        help="Dir to which output will be saved (default: %(default)s).",
+    )
+
+    args = argparser.parse_args()
+
+    data_parser.main(output_dir=args.output_dir, input_dir=args.input_dir)
+
+
+if __name__ == "__main__":
+    main()
